18ee8584503046ef46515cf7425e49ee09f64d54
Alexa201228 <aleksandra.gr2012@gmail.com>
2021-09-20 08:16:16 +0300

    fix issue with redirect to login

--- chatroom/chat/urls.py
+++ chatroom/chat/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('chat/', views.chat_selection, name='chat_selection'),
     path('chat/<str:room_title>/', views.room, name='room'),
+    path('login-chat/', views.login_chat, name='login'),
     path(r'login-chat/(?P<forbidden>\w+)/$', views.login_chat, name='login'),
 ]
